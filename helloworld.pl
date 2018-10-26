8e59058ac5602c16cf593072b163e86a8728801a
Joao Batista <batista.freebsd@gmail.com>
2018-10-26 10:27:20 -0300

    Update to hello world keep nice...

---
+++ helloworld.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl
+#
+# @File helloworld.pl
+# @Author batista
+# @Created 26/10/2018 00:23:48
+#
+
+print "\n\nHello World From Perl....!!!\n\n";
+
+# Output
+#-----------------------------
+#/usr/bin/perl ./helloworld.pl
+
+
+#Hello World From Perl....!!!
