12a0660457df4d56c3d1dc0f3a22315edbed3cf8
Joao Batista <batista.freebsd@gmail.com>
2018-10-26 15:01:30 -0300

    Example hash data to structures...

---
+++ hashdatastructs.pl
@@ -0,0 +1,61 @@
+#!/usr/bin/perl
+#
+# @File hashdatastructs.pl
+# @Author batista
+# @Created 26/10/2018 11:40:32
+#
+use Term::ANSIColor qw(:constants);
+
+
+%data = ('Mary',20,'John',25,'Jenny',17, 'Paul',21);
+#print "$data{'Mary'}\n";//
+
+@vname = keys %data;# Keys to string
+@vages = values %data;#Value to Integer
+
+print "-----------------------\n";
+ 
+print "|", BLUE, ON_CYAN, "       Names:       ", RESET,"|\n";
+print "-----------------------\n";
+print "$vname[0]\n";#print name by position
+print "$vname[1]\n";#print name by position
+print "$vname[2]\n";#print name by position
+print "$vname[3]\n";#print name by position
+print "[          END         ]\n";
+
+print "\n\n----------------------------\n";
+print "|", BLUE, ON_CYAN, "       Ages:       ", RESET,"|\n";
+print "----------------------------\n";
+print "$vages[0]\n";#print age by position
+print "$vages[1]\n";#print age by position
+print "$vages[2]\n";#print age by position
+print "$vages[3]\n";#print age by position
+print "[          END             ]\n";
+
+print "\n\n--------------------------------\n";
+print "|",BLUE, ON_CYAN,"   Print Name with Ages:      ",RESET,"|\n";
+print "--------------------------------\n";
+
+print "Name:......:";
+print BLUE,"$vname[0]", RESET;
+print ", Age:......:";
+print RED, "$vages[0]", RESET, "\n";#print name and age by position
+
+print "Name:......:";
+print BLUE,"$vname[1]", RESET;
+print ", Age:......:";
+print RED, "$vages[1]", RESET, "\n";#print name and age by position
+
+print "Name:......:";
+print BLUE,"$vname[2]", RESET;
+print ", Age:.....:";
+print RED, "$vages[2]", RESET, "\n";#print name and age by position
+
+print "Name:......:";
+print BLUE,"$vname[3]", RESET;
+print ", Age:......:";
+print RED, "$vages[3]", RESET, "\n";#print name and age by position
+
+print "\n\n[             END              ]\n";
+
+
