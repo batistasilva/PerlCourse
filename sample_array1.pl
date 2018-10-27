a7ff65bc7a629f1bfb7e66f7c089720563f356f7
Joao Batista <batista.freebsd@gmail.com>
2018-10-26 23:51:54 -0300

    Sample array 1 to use with week date...

---
+++ sample_array1.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl
+#
+# @File sample_array1.pl
+# @Author batista
+# @Created 26/10/2018 23:48:08
+#
+use Term::ANSIColor qw(:constants);
+use warnings;
+use strict;
+ 
+my @days = qw(Mon Tue Wed Thu Fri Sat Sun);
+print "\nDays...:" ,BLUE, "@days" ,"\n", RESET;
+
+
+
