ecc3daabe987e51bb3f426df7c86e47e292e1bf7
Joao Batista <batista.freebsd@gmail.com>
2018-10-26 23:55:58 -0300

    Sample array 2, Accessing Perl array elements...

---
+++ sample_array2.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl
+#
+# @File sample_array2.pl
+# @Author batista
+# @Created 26/10/2018 23:53:19
+#
+use Term::ANSIColor qw(:constants);
+use warnings;
+use strict;
+ 
+my @days = qw(Mon Tue Wed Thu Fri Sat Sun);
+print "\nDay...:", BLUE, $days[0], RESET;
+ 
+print("\n");
